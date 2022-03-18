0656081cfa99c9e36cd259bfc7d11532279996d5
zhmzm <365460860@qq.com>
2022-03-18 22:17:24 +0800

    update

--- src/server.py
+++ src/server.py
@@ -7,48 +7,20 @@ import torch.nn.functional as F
 from torch import optim
 from Models import Mnist_2NN, Mnist_CNN
 from clients import ClientsGroup, client
-import matplotlib.pyplot as plt
-def cos(a,b):
-    res = np.sum(a*b.T)/((np.sqrt(np.sum(a * a.T)) + 1e-9) * (np.sqrt(np.sum(b * b.T))) + 1e-9)
-    '''relu'''
-    if res < 0:
-        res = 0
-    return res
-def model2vector(model):
-    nparr = np.array([])
-    for key, var in model.items():
-        nplist = var.cpu().numpy()
-        nplist = nplist.ravel()
-        nparr = np.append(nparr, nplist)
-    return nparr
-def cosScoreAndClipValue(net1, net2):
-    '''net1 -> centre, net2 -> local, net3 -> early model'''
-    vector1 = model2vector(net1)
-    vector2 = model2vector(net2)
 
-    return cos(vector1, vector2), norm_clip(vector1, vector2)
-def norm_clip(nparr1, nparr2):
-    '''v -> nparr1, v_clipped -> nparr2'''
-    vnum = np.linalg.norm(nparr1, ord=None, axis=None, keepdims=False) + 1e-9
-    return vnum / np.linalg.norm(nparr2, ord=None, axis=None, keepdims=False) + 1e-9
 
-def get_weight(update, model):
-    '''get the update weight'''
-    for key, var in update.items():
-        update[key] -= model[key]
-    return update
 parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter, description="FedAvg")
 parser.add_argument('-g', '--gpu', type=str, default='0', help='gpu id to use(e.g. 0,1,2,3)')
 parser.add_argument('-nc', '--num_of_clients', type=int, default=100, help='numer of the clients')
 parser.add_argument('-cf', '--cfraction', type=float, default=0.1, help='C fraction, 0 means 1 client, 1 means total clients')
 parser.add_argument('-E', '--epoch', type=int, default=5, help='local train epoch')
 parser.add_argument('-B', '--batchsize', type=int, default=10, help='local train batch size')
-parser.add_argument('-mn', '--model_name', type=str, default='mnist_cnn', help='the model to train')
+parser.add_argument('-mn', '--model_name', type=str, default='mnist_2nn', help='the model to train')
 parser.add_argument('-lr', "--learning_rate", type=float, default=0.01, help="learning rate, \
                     use value from origin paper as default")
-parser.add_argument('-vf', "--val_freq", type=int, default=2, help="model validation frequency(of communications)")
-parser.add_argument('-sf', '--save_freq', type=int, default=100, help='global model save frequency(of communication)')
-parser.add_argument('-ncomm', '--num_comm', type=int, default=2000, help='number of communications')
+parser.add_argument('-vf', "--val_freq", type=int, default=5, help="model validation frequency(of communications)")
+parser.add_argument('-sf', '--save_freq', type=int, default=20, help='global model save frequency(of communication)')
+parser.add_argument('-ncomm', '--num_comm', type=int, default=1000, help='number of communications')
 parser.add_argument('-sp', '--save_path', type=str, default='./checkpoints', help='the saving path of checkpoints')
 parser.add_argument('-iid', '--IID', type=int, default=0, help='the way to allocate data to clients')
 
@@ -62,7 +34,6 @@ if __name__=="__main__":
     args = parser.parse_args()
     args = args.__dict__
 
-    acc_list=[]
     test_mkdir(args['save_path'])
 
     os.environ['CUDA_VISIBLE_DEVICES'] = args['gpu']
@@ -98,33 +69,19 @@ if __name__=="__main__":
         clients_in_comm = ['client{}'.format(i) for i in order[0:num_in_comm]]
 
         sum_parameters = None
-        FLTrustTotalScore = 0
-        FLTrustCentralNorm = myClients.centralTrain(args['epoch'], args['batchsize'], net,
-                                                                         loss_func, opti, global_parameters)
-        '''get the update weight'''
-        FLTrustCentralNorm = get_weight(FLTrustCentralNorm, global_parameters)
-
         for client in tqdm(clients_in_comm):
             local_parameters = myClients.clients_set[client].localUpdate(args['epoch'], args['batchsize'], net,
                                                                          loss_func, opti, global_parameters)
-            '''get the update weight'''
-            local_parameters = get_weight(local_parameters, global_parameters)
-            #计算cos相似度得分和向量长度裁剪值
-            client_score, client_clipped_value = cosScoreAndClipValue(FLTrustCentralNorm, local_parameters)
-
-            FLTrustTotalScore += client_score
             if sum_parameters is None:
                 sum_parameters = {}
                 for key, var in local_parameters.items():
-                    #乘得分 再乘裁剪值
-                    sum_parameters[key] = client_score * client_clipped_value * var.clone()
+                    sum_parameters[key] = var.clone()
             else:
                 for var in sum_parameters:
-                    sum_parameters[var] = sum_parameters[var] + client_score * client_clipped_value * local_parameters[var]
+                    sum_parameters[var] = sum_parameters[var] + local_parameters[var]
 
         for var in global_parameters:
-            #除以所以客户端的信任得分总和
-            global_parameters[var] += (sum_parameters[var] / FLTrustTotalScore + 1e-9)
+            global_parameters[var] = (sum_parameters[var] / num_in_comm)
 
         with torch.no_grad():
             if (i + 1) % args['val_freq'] == 0:
@@ -138,8 +95,6 @@ if __name__=="__main__":
                     sum_accu += (preds == label).float().mean()
                     num += 1
                 print('accuracy: {}'.format(sum_accu / num))
-                acc_list.append(sum_accu.item() / num)
-                print(acc_list)
 
         if (i + 1) % args['save_freq'] == 0:
             torch.save(net, os.path.join(args['save_path'],
@@ -149,6 +104,4 @@ if __name__=="__main__":
                                                                                                 args['learning_rate'],
                                                                                                 args['num_of_clients'],
                                                                                                 args['cfraction'])))
-    print(acc_list)
-    plt.plot(acc_list)
-    plt.show()
+
--- src/test.py
+++ src/test.py
@@ -1,5 +1,6 @@
 # -*- coding = utf-8 -*-
 import torch
+import matplotlib.pyplot as plt
 import numpy as np
 def cos(a,b):
     res = np.sum(a*b.T)/(np.sqrt(np.sum(a * a.T)) * np.sqrt(np.sum(b * b.T)))
@@ -34,7 +35,11 @@ def cosScore(net1, net2):
     return cos(nparr1, nparr2)
 # tor_arr=torch.from_numpy(np_arr)
 # tor2numpy=tor_arr.numpy()
-dict = './checkpoints/mnist_2nn_num_comm19_E5_B10_lr0.01_num_clients100_cf0.1'
-model1 = torch.load(dict)
-model2 = torch.load('./checkpoints/mnist_2nn_num_comm299_E5_B10_lr0.01_num_clients100_cf0.1')
-print(norm_clip(model1,model2))
+# dict = './checkpoints/mnist_2nn_num_comm19_E5_B10_lr0.01_num_clients100_cf0.1'
+# model1 = torch.load(dict)
+# model2 = torch.load('./checkpoints/mnist_2nn_num_comm299_E5_B10_lr0.01_num_clients100_cf0.1')
+# print(norm_clip(model1,model2))
+acc_list =[0.18149993896484376, 0.556499900817871, 0.6716998291015625, 0.7579998016357422, 0.8413002014160156, 0.8471998596191406, 0.9054999542236328, 0.8280001831054687, 0.8501999664306641, 0.9144998931884766, 0.8991999816894531, 0.9091998291015625, 0.8923001098632812, 0.8963998413085937, 0.9419001007080078, 0.9487999725341797, 0.9246000671386718, 0.9383002471923828, 0.9163002777099609, 0.9444000244140625, 0.9511000061035156, 0.95010009765625, 0.9483001708984375, 0.9571001434326172, 0.9567000579833984, 0.9513001251220703, 0.9598001098632812, 0.9613999176025391, 0.9667999267578125, 0.938499755859375, 0.9564000701904297, 0.9660997772216797]
+
+plt.plot(acc_list)
+plt.show()
